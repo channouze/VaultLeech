92f82420510f0e5db070f5cf8241746370b554c5
channouze <ch4ndl3r@gmail.com>
2017-04-04 01:06:30 -0400

    VaultLeech
    + build xml file from URL
    + retrieve talk, duration and speaker info

--- VaultLeech.py
+++ VaultLeech.py
@@ -1,67 +1,108 @@
-import urllib
-import urllib2
-import cookielib
-import re
 import os
 import requests
+import sys
+from lxml import etree
 
-username = ''
-password = ''
-url = 'https://romo.ovh'
+version = '0.0.1'
 
 class VaultLeech(object):
 
-    def __init__(self, login, password, talkurl):
+    def __init__(self, login, password, talkurl, loginurl):
         """ Start up... """
         self.login = login
         self.password = password
         self.talkurl = talkurl
-        self.loginurl = 'http://ddl.romo.ovh/login.php'
+        self.loginurl = loginurl
 
-        if(self.login is None or self.password is None or self.talkurl is None):
-            print 'usage: VaultLeech e-mail password talkURL'
-        else:
-            print self.login, self.password, self.talkurl
-        
-        self.cj = cookielib.CookieJar()
-        self.opener = urllib2.build_opener(
-            urllib2.HTTPRedirectHandler(),
-            urllib2.HTTPHandler(debuglevel=0),
-            urllib2.HTTPSHandler(debuglevel=0),
-            urllib2.HTTPCookieProcessor(self.cj)
-        )
-        self.opener.addheaders = [
-            ('User-agent', ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'))
-        ]
-        # need this twice - once to set cookies, once to log in...
-        self.loginToVault()
-        self.loginToVault()
+        self.gdcYear = '2000'
+
+        # todo verify args
+        print 'VaultLeech v' + version + '\nA GDC Vault Backup Tool\nUsage: VaultLeech user pass url'
 
     def loginToVault(self):
-        """
-        Handle login. This should populate our cookie jar.
-        """
-        login_data = urllib.urlencode({
-            'Email' : self.login,
-            'password' : self.password,
-        })
-        # response = self.opener.open(self.loginurl, login_data)
-        # return ''.join(response.readlines())
+        r = requests.get(self.loginurl, auth=(self.login, self.password))
+        self.checkURLResponse(r)
 
     def validateEmail(self):
-        print 'it works'
+        print 'todo'
+
+    def validateTalkURL(self):
+        print 'todo'
 
     def buildPathToVideo(self):
-        print 'wip'
+        r = requests.get(self.talkurl)
+        self.checkURLResponse(r)
+
+        # find the mp4 xml definition file
+
+        for line in r.iter_lines():
+            if 'iframe' in line:
+                break
+
+        # build the xml request url
+        
+        # outputs http://evt.dispeak.com/ubm/gdc/sf17/player.html?xml=847415_GXDS.xml
+        start = line.find("http")
+        end = line.find(".xml&") - len(line) + 4
+
+        xmlURL = line [start:end]
+
+        # outputs http://evt.dispeak.com/ubm/gdc/sf17/
+        baseURL = xmlURL[:xmlURL.find('player.html')]
+        
+        # outputs player.html?xml=847415_GXDS.xml
+        xmlVideo = xmlURL.rsplit('/')[-1]
+
+        # outputs 847415_GXDS.xml
+        xmlVideo = xmlVideo[xmlVideo.find('xml=')+4:]
+
+        # builds the final xml url
+        # outputs http://evt.dispeak.com/ubm/gdc/sf17/xml/847415_GXDS.xml
+        xmlRequest = baseURL + 'xml/' + xmlVideo
+        
+        # find the mp4 files
+        rxml = requests.get(xmlRequest)
+
+        self.showDetails(xmlRequest)
+
+        for line in r.iter_lines():
+            if 'streamName' in line:
+                print line
+
+    
+    # show video details    
+    def showDetails(self, xml):
+        tree = etree.parse(xml)
+        print '=================================='
+        year = xml[xml.find('sf')+2:xml.find('sf')+4]
+        if (int(year)+1000 > 1096) and (int(year)+1000 < 1000):
+            stryear = 1900+int(year)
+        else:
+            stryear = 2000+int(year)
+        print 'GDC', stryear
+        for title in tree.xpath('/podiumPresentation/metadata/title'):
+            print title.text
+
+        for speaker in tree.xpath('/podiumPresentation/metadata/speaker'):
+            print 'Speaker(s): ' + speaker.text
+
+        for talkLength in tree.xpath('/podiumPresentation/metadata/endTime'):
+            print 'Duration: ' + talkLength.text
+        print '=================================='
+
+    def checkURLResponse(self, req):
+        if (req.status_code != 200):
+            print 'something went wrong'
 
     def getVideo(self):
         
-        link = 'https://romo.ovh'
+        link = 'https://romo.ovh/romo.png'
         file_name = 'romo.png'
 
         with open(file_name, "wb") as f:
             print "Downloading %s" % file_name
-            response = requests.get(link, stream=True)
+            # TODO implement SSL verify the proper way
+            response = requests.get(link, stream=True, verify=False)
             total_length = response.headers.get('content-length')
 
             if total_length is None: # no content length header
@@ -76,5 +117,3 @@ class VaultLeech(object):
                     sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
                     sys.stdout.flush()
 
-vaultleecher=VaultLeech(username, password, url)
-vaultleecher.getVideo()
