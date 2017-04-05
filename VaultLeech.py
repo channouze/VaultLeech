2894008b8ab371326ac25b1436f8f4f410a7b84b
channouze <ch4ndl3r@gmail.com>
2017-04-04 22:51:44 -0400

    Updated VaultLeech
    First functional version!
    Works with free videos from GDC 2013 to 2017

--- VaultLeech.py
+++ VaultLeech.py
@@ -7,15 +7,16 @@ version = '0.0.1'
 
 class VaultLeech(object):
 
-    def __init__(self, login, password, talkurl, loginurl):
+    def __init__(self, talkurl, login, password):
         """ Start up... """
         self.login = login
         self.password = password
         self.talkurl = talkurl
-        self.loginurl = loginurl
+        
+        self.loginurl = ''
 
-        # todo verify args
-        print 'VaultLeech v' + version + '\nA GDC Vault Backup Tool\nUsage: VaultLeech user pass url'
+        # TODO: verify args
+        print 'VaultLeech v' + version + '\nA GDC Vault Backup Tool\nUsage: VaultLeech url (user) (pass)'
 
     def loginToVault(self):
         r = requests.get(self.loginurl, auth=(self.login, self.password))
@@ -32,7 +33,6 @@ class VaultLeech(object):
         self.checkURLResponse(r)
 
         # find the mp4 xml definition file
-
         for line in r.iter_lines():
             if 'iframe' in line:
                 break
@@ -53,27 +53,39 @@ class VaultLeech(object):
 
         # outputs 847415_GXDS.xml
         xmlVideo = xmlVideo[xmlVideo.find('xml=')+4:]
+        # xmlVideo = xmlVideo.split('=')
 
         # builds the final xml url
         # outputs http://evt.dispeak.com/ubm/gdc/sf17/xml/847415_GXDS.xml
         xmlRequest = baseURL + 'xml/' + xmlVideo
         
-        # find the mp4 files
-        rxml = requests.get(xmlRequest)
+        # find the mp4 files in xml
+        # outputs   mp4:assets/ubm/gdc/sf17/847254_WTDP-v9f81f-500.mp4
+        #           mp4:assets/ubm/gdc/sf17/847254_WTDP-v9f81f-800.mp4
+        #           mp4:assets/ubm/gdc/sf17/847254_WTDP-v9f81f-1300.mp4
+        tree = etree.parse(xmlRequest)
+        mp4list = []
+        for mp4 in tree.xpath('/podiumPresentation/metadata/MBRVideos/MBRVideo/streamName'):
+            mp4list.append(mp4.text)
+        
+        # TODO: build host from js file in player.html
+        host = 'http://s3-2u-d.digitallyspeaking.com'
 
-        self.showDetails(xmlRequest)
+        # joins host and mp4 file
+        urls = []
 
-        for line in r.iter_lines():
-            if 'streamName' in line:
-                print line
-
+        for index in range (0,len(mp4list)):
+            urls.append(host + '/' + str(mp4list[index])[4:])
+        
+        # display download details (if found)
+        self.showDetails(xmlRequest, urls)
     
     # show video details    
-    def showDetails(self, xml):
+    def showDetails(self, xml, filelist):
         tree = etree.parse(xml)
-        print '=================================='
+        print '='*50
         year = xml[xml.find('sf')+2:xml.find('sf')+4]
-        if (int(year)+1000 > 1096) and (int(year)+1000 < 1000):
+        if (int(year)+1000 > 1096):
             stryear = 1900+int(year)
         else:
             stryear = 2000+int(year)
@@ -86,21 +98,46 @@ class VaultLeech(object):
 
         for talkLength in tree.xpath('/podiumPresentation/metadata/endTime'):
             print 'Duration: ' + talkLength.text
-        print '=================================='
+        
+        print '='*50
+        # prints the files available and the bitrate information
+        print 'Available files:\n'
+        # TODO: bitrate information
+        bitrate = [0,1,2]
+        size = [0,1,2]
+        for index in range (0,len(filelist)):
+            # print str(index)+':', filelist[index].rsplit('/')[-1], 'Bitrate: ', bitrate[index], 'Size: ', size[index]
+            print str(index)+':', filelist[index].rsplit('/')[-1]
+
+        # TODO: Accept input from user
+        # TODO: Check input from user
+        
+        # Finally, Get video
+        self.getVideo(filelist[0], stryear, title.text)
+
+        return None
 
     def checkURLResponse(self, req):
         if (req.status_code != 200):
             print 'something went wrong'
 
-    def getVideo(self):
+    def getVideo(self, link, year, name):
         
-        link = 'https://romo.ovh/romo.png'
-        file_name = 'romo.png'
+        # TODO Beautify the file name
+        
+#        file_name = 'GDC'+str(year)+' '+name+'.mp4'
+#
+#        print file_name
+#        for char in file_name:
+#            if char.isalnum() or char == ' ':
+
+        file_name = link.rsplit('/')[-1]        
 
         with open(file_name, "wb") as f:
-            print "Downloading %s" % file_name
-            # TODO implement SSL verify the proper way
-            response = requests.get(link, stream=True, verify=False)
+            print "\nDownloading %s" % file_name
+            # TODO: implement SSL verify the proper way
+            # TODO: headers and referer to a variable
+            response = requests.get(link, stream=True, verify=False, headers={'Referer': 'http://evt.dispeak.com/ubm/gdc/sf17/player.html', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'})
             total_length = response.headers.get('content-length')
 
             if total_length is None: # no content length header
@@ -114,4 +151,5 @@ class VaultLeech(object):
                     done = int(50 * dl / total_length)
                     sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
                     sys.stdout.flush()
+            print '\n'
 
