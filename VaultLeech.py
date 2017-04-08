91644b3ebd96e8b55cda15bf59159d6135935cfc
channouze <ch4ndl3r@gmail.com>
2017-04-08 18:58:10 -0400

    Groundwork for auth-based videos + minor improvements
    - Validate url and e-mail provided
    - Login validation mechanism
    - Display filesize for each videos
    - Display download speed

--- VaultLeech.py
+++ VaultLeech.py
@@ -1,11 +1,14 @@
 import os
 import requests
 import sys
+import time
+import json
 from lxml import etree
 from validate_email import validate_email
 
-version = '0.0.1d'
+version = '0.0.1e'
 vaultLoginURL = 'http://gdcvault.com/api/login.php'
+vaultLogoutURL = 'http://gdcvault.com/logout'
 
 class VaultLeech(object):
 
@@ -15,41 +18,82 @@ class VaultLeech(object):
         print 'VaultLeech v' + version + '\nA GDC Vault Backup Tool\nUsage: VaultLeech url (user) (pass)'
         # print 'VaultLeech v' + version + '\nA GDC Vault Backup Tool\nUsage: VaultLeech url'
 
-        # TODO: verify args
         # First: verify the URL so we don't abuse the login API
-        if self.validateTalkURL(talkurl):
+        if self.isTalkURLValid(talkurl):
             if login is None and password is None:
                 # Means we handle a free video
                 self.buildPathToVideo(talkurl)
             else:
-                if self.validateEmail(login):
+                if self.isEmailValid(login):
                     # Means we deal with authenticated stuff
                     if self.loginToVault(login,password):
                         self.buildPathToVideo(talkurl)
+                        self.logoutFromVault
                 else:
-                    print 'e-mail is not valid, please try again.'
-                print 'url supplied is not a valid talk url, please try again'
+                    print 'ERROR: e-mail is not valid, please try again.'
+        else:
+             print 'ERROR: url supplied is not a valid talk url, please try again'
 
     def loginToVault(self, login, password):
-        r = requests.get(vaultLoginURL, auth=(login, password))
+        # first make sure we're not already logged in
+        r = requests.get('http://www.gdcvault.com/')
         self.checkURLResponse(r)
+
+        for logoutData in r.iter_lines():
+            if 'Logout' in logoutData:
+                # print "User already logged in"
+                self.logoutFromVault()
+                break
+
+        # if not, log in using the user supplied credentials
+        payload = {'email':login,'password':password}
+        r = requests.post(vaultLoginURL, data=payload)
+        self.checkURLResponse(r)
+
+        # our request gets us a JSON file so we can now check credentials
+        data = r.json()
+
+        if data['is_bump']:
+            print 'ERROR: You are already logged in with that account from another session or computer'
+            exit(0)
+
+        if data["isSubscribed"]:
+            print 'Logged in as', data['first_name'], data['last_name'], 'from', data['company']
+            print 'This account subscription expires on', data['expiration'].rsplit()[0]
+
+#        with open('log_auth.txt', "wb") as mylogfile:
+#            mylogfile.write(r.text)
+
         return True
 
-    def validateEmail(self, email):
+    def logoutFromVault(self):
+        r = requests.get(vaultLogoutURL)
+        self.checkURLResponse(r)
+
+    def isEmailValid(self, email):
         return validate_email(str(email))
 
-    def validateTalkURL(self, url):
-        return True
+    def isTalkURLValid(self, url):
+        if url.startswith('http://www.gdcvault.com/play') or url.startswith('http://gdcvault.com/play'):
+            return True
+        else:
+            return False
 
     def buildPathToVideo(self, talkurl):
 
         r = requests.get(talkurl)
         self.checkURLResponse(r)
 
+        with open('log_talk.txt', "wb") as mylogfile:
+            mylogfile.write(r.text)
+
         # find the mp4 xml definition file
         for line in r.iter_lines():
             if 'iframe' in line:
                 break
+        if not 'iframe' in line:
+            print 'ERROR: Could not find requested talk. Make sure the URL is valid'
+            exit (0)
 
         # build the xml request url
         
@@ -119,7 +163,7 @@ class VaultLeech(object):
                 if 'httpHostSource' in line:
                     break
             if 'httpHostSource' not in line:
-                sys.exit('failed to find the host, aborting...')
+                sys.exit('ERROR: failed to find the host, aborting...')
 
             host = line.rsplit('=')[-1]
             host = host[2:-2]
@@ -142,6 +186,8 @@ class VaultLeech(object):
         
         # display download details (if found)
         self.showDetails(xmlRequest, urls)
+
+        self.logoutFromVault()
     
     # show video details    
     def showDetails(self, xml, filelist):
@@ -162,14 +208,26 @@ class VaultLeech(object):
             print 'Duration: ' + talkLength.text
         
         print '='*50
-        # prints the files available and the bitrate information
+        # prints the files available
         print 'Available files:\n'
-        # TODO: bitrate & size information
-        # bitrate = [0,1,2]
-        # size = [0,1,2]
+
+        bitrate = []
+        for xmlbitrate in tree.xpath('/podiumPresentation/metadata/MBRVideos/MBRVideo/bitrate'):
+            bitrate.append(xmlbitrate.text)
+        # TODO: handle the flv NULL size & bitrate
+        if len(bitrate) == 1:
+            pass
+
+        size = []
+
+        for filesize in filelist:
+            r = requests.head(filesize)
+            headers = r.headers
+            size.append(int(headers.get('content-length'))//(1024*1024))
+
         for index in range (0,len(filelist)):
-            # print str(index)+':', filelist[index].rsplit('/')[-1], 'Bitrate: ', bitrate[index], 'Size: ', size[index]
-            print str(index)+':', filelist[index].rsplit('/')[-1]
+            # Displays the list of files along with bitrate and Size
+            print str(index)+':', filelist[index].rsplit('/')[-1], bitrate[index], 'kbps  Size: ', size[index], 'MB'
 
         if len(filelist) == 1 and filelist[len(filelist)-1].endswith('.flv'):
             print '\nWARNING! Download is not supported at the moment for flv videos'
@@ -229,7 +287,7 @@ class VaultLeech(object):
 
         return event.upper()
     
-    # return the year of the event from the video url
+    # returns the year of the event from the video url
     def getYear(self, string):
 
         year = self.getInformationFromURL(string) [-2:]
@@ -260,6 +318,7 @@ class VaultLeech(object):
 
     def checkURLResponse(self, req):
         if (req.status_code != 200):
+            print 'Error', req.status_code
             sys.exit('something went wrong')
 
     def getVideo(self, link, year, name):
@@ -271,7 +330,8 @@ class VaultLeech(object):
         with open(file_name, "wb") as f:
             print "\nDownloading %s" % file_name
             # TODO: implement SSL verify the proper way
-            # TODO: headers and referer to a variable, built from source url
+            # TODO: headers and referer to a generated variable, built from source url
+            start = time.clock()
             response = requests.get(link, stream=True, verify=False, headers={'Referer': 'http://evt.dispeak.com/ubm/gdc/sf17/player.html', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'})
             total_length = response.headers.get('content-length')
 
@@ -280,10 +340,11 @@ class VaultLeech(object):
             else:
                 dl = 0
                 total_length = int(total_length)
-                for data in response.iter_content(chunk_size=4096):
-                    dl += len(data)
-                    f.write(data)
+                for chunk in response.iter_content(chunk_size=4096):
+                    dl += len(chunk)
+                    f.write(chunk)
                     done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                    # TODO: Display adaptive download speed
+                    sys.stdout.write("\r[%s%s] %s Kbps" % ('=' * done, ' ' * (50-done), (dl//(time.clock() - start)//1000)))    
                     sys.stdout.flush()
-            print '\n'
+            sys.stdout.write('\n\nWrote %s%s') % (os.getcwd(), file_name)
